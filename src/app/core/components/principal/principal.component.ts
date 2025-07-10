29bb73c03ccabca9f529cc3b8edf2dfd09e77837
Vahlokillo <alextornerorobles@gmail.com>
2025-07-11 00:39:59 +0200

    FEAT: principal

---
+++ src/app/core/components/principal/principal.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { SupabaseService } from '../../services/supabase/supabase.service';
+
+interface OpcionMenu {
+  icono: string,
+  titulo: string,
+  redirect: string
+}
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.scss']
+})
+export class PrincipalComponent {
+
+  public listaOpciones: OpcionMenu[] = [];
+  public user: any;
+
+  constructor(private _title: Title, private _router: Router, private _supabase: SupabaseService) { }
+
+  async ngOnInit() {
+    this._title.setTitle('Menú principal');
+    this.user = this._supabase.getUser();
+    this.conseguirOpcionesMenu();
+  }
+
+  conseguirOpcionesMenu() {
+    this.listaOpciones = [
+      // { titulo: 'TPV', icono: 'shopping_cart', redirect: '' },
+      { titulo: 'Oficina', icono: 'business_center', redirect: '/articulos' }
+    ]
+  }
+
+  redirigirPantalla(ruta: string) {
+    if (ruta !== '') this._router.navigate([ruta]);
+  }
+
+  cerrarSesion() {
+    this._supabase.signOut();
+    this._router.navigate(['/login']);
+  }
+}
