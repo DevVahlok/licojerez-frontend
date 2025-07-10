2e11ce683afb0b9f09aa965fb252e4964af76455
Vahlokillo <alextornerorobles@gmail.com>
2025-07-11 00:39:49 +0200

    FEAT: main

---
+++ src/app/core/components/main/main.component.ts
@@ -0,0 +1,42 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { SupabaseService } from '../../services/supabase/supabase.service';
+
+interface Usuario {
+  name: string,
+  user: string
+}
+
+interface OpcionMenuLateral {
+  title: string,
+  url: string,
+  iconFont: string,
+  icon: string,
+}
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MainComponent {
+
+  public user: any;
+  public opcionesMenuLateral: OpcionMenuLateral[] = [{ title: 'Artículos', icon: 'liquor', iconFont: 'material-symbols-outlined', url: '/articulos' }]
+
+  constructor(private _router: Router, public _dialog: MatDialog, private _supabase: SupabaseService) { }
+
+  ngOnInit() {
+    this.user = this._supabase.getUser();
+  }
+  abrirDialogMenuLateral() {
+
+  }
+
+  cerrarSesion() {
+    this._supabase.signOut();
+    this._router.navigate(['/login']);
+  }
+}
