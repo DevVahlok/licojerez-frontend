59ad674ce8203946ece065d77f3527fa3ca559ea
Vahlokillo <alextornerorobles@gmail.com>
2025-07-11 00:40:51 +0200

    FEAT: tabla tabulator

---
+++ src/app/core/services/utils-v2/utils.service.ts
@@ -0,0 +1,38 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { saveAs } from 'file-saver';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UtilsService {
+  public eventoMenuContextual: EventEmitter<any> = new EventEmitter();
+  public eventoGetBanners: EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+  ordenarObjectArrayPorPropiedad(arr: Array<object>, propiedad: string): Array<any> | undefined {
+
+    if (arr !== undefined) {
+      return arr.sort((a: any, b: any) => Number(a[propiedad]) < Number(b[propiedad]) ? -1 : (Number(a[propiedad]) > Number(b[propiedad]) ? 1 : 0));
+    } else {
+      return undefined;
+    }
+
+  }
+
+  downloadElementToPng(element: HTMLElement, title?: string): Blob | null {
+    html2canvas(element).then((canvas: any) => {
+      canvas.toBlob((blob: Blob) => {
+        saveAs(blob, `${title ? title : 'Tabla'}.png`);
+        return blob;
+      });
+    });
+    return null;
+  }
+
+  delay(ms: number): Promise<any> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+}
