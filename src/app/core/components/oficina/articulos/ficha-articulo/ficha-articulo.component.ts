884c80822df2dcf9e6e41b9db963c5640f03d636
Vahlokillo <alextornerorobles@gmail.com>
2025-08-24 04:06:02 +0200

    FEAT: editar comisiones artículo

--- src/app/core/components/oficina/articulos/ficha-articulo/ficha-articulo.component.ts
+++ src/app/core/components/oficina/articulos/ficha-articulo/ficha-articulo.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ListaDesplegablesFichaArticulo } from '../articulos.component';
 import { Articulo, Familia, IVA, Marca, Proveedor, Subfamilia } from 'src/app/models/oficina';
 import { RealtimeChannel } from '@supabase/supabase-js';
@@ -8,8 +8,14 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { SupabaseService } from 'src/app/core/services/supabase/supabase.service';
 import { from } from 'rxjs';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DialogConfirmacion } from 'src/app/shared/dialogs/dialog-confirmacion/dialog-confirmacion';
+
+interface Vendedor {
+  codigo: number,
+  nombre: string,
+  comision: number
+}
 
 @Component({
   selector: 'app-ficha-articulo',
@@ -38,7 +44,7 @@ export class FichaArticuloComponent {
     marca: [],
   }
 
-  public listaVendedores: any = [];
+  public listaVendedores: Vendedor[] = [];
   public listaVendedoresFiltrada: any = [];
   public inputVendedor = new FormControl('');
   public listasDesplegables: ListaDesplegablesFichaArticulo = {
@@ -69,12 +75,16 @@ export class FichaArticuloComponent {
     id_iva: new FormControl(-1, Validators.required),
     margen: new FormControl(0, Validators.required),
     activo: new FormControl(false, Validators.required),
-    comision_default: new FormControl(0),
+    comision_default: new FormControl(0, Validators.pattern(/^-?\d+(\.\d+)?$/)),
     tiene_lote: new FormControl(false, Validators.required),
     id_marca: new FormControl(-1),
     formato: new FormControl(null)
   });
   @Output() cambiarTab = new EventEmitter<number>();
+  public listaVendedoresDialog: Vendedor[];
+  public dialogRef: MatDialogRef<any>;
+  @ViewChild('dialogEditarComision') dialogEditarComision: TemplateRef<any>;
+  public comisionDefaultDialog = 0;
 
   constructor(public _router: Router, public _supabase: SupabaseService, protected _snackbar: MatSnackBar, private _dialog: MatDialog) { }
 
@@ -138,7 +148,7 @@ export class FichaArticuloComponent {
   }
 
   async getVendedores() {
-    const { data, error } = await this._supabase.supabase.from('comisiones_articulos').select(`id_comision, comision, vendedores (*)`).eq('id_articulo', this.id).order('comision');
+    const { data, error } = await this._supabase.supabase.from('comisiones_articulos').select(`id_comision, comision, vendedores (*)`).eq('id_articulo', this.id).order('comision', { ascending: false });
 
     if (error) {
       //TODO: gestionar errores
@@ -168,11 +178,13 @@ export class FichaArticuloComponent {
 
     //TODO: aplicar sockets a cada desplegable (cada vez que se crea una subfamilia, por ejemplo), también en lista vendedores
 
-    //TODO: poder ordenar lista vendedores por % comisión
+    //TODO: aplicar socket a la tabla de artículos (comprobar con edición y baja, por ejemplo)
 
-    //TODO: relacionar jerarquicamente familias con subfamilias, bloquear subfamilia si no tiene familia seleccionada
+    //TODO: apartado comisiones (añadir, editar...)
 
-    //TODO: poner RLS policies en All
+    //TODO: apartado etiquetas
+
+    //TODO: apartado crear artículo
 
     this.suscripcionArticulo = this._supabase.supabase.channel(`articulo-${this.id}`).on('postgres_changes', { event: '*', schema: 'public', table: 'articulos', filter: `codigo=eq.${this.id}` }, payload => {
       this.articulo = payload.new as Articulo;
@@ -189,13 +201,15 @@ export class FichaArticuloComponent {
     this.articulo = data;
     this.spinner = false;
 
-    this.formArticulo.setValue(this.tratamientoPreFormulario())
-    this.valoresAnteriores = this.tratamientoPreFormulario();
+    const datosForm = this.tratamientoPreFormulario();
+    this.formArticulo.setValue(datosForm);
+    this.valoresAnteriores = datosForm;
+
+    this.gestionarFormulario();
     this.getListasDesplegables();
   }
 
-  tratamientoPreFormulario() {
-
+  gestionarFormulario() {
     this.formArticulo.get('id_articulo')?.disable();
     this.formArticulo.get('fecha_alta')?.disable();
     this.formArticulo.get('precio_coste')?.disable();
@@ -212,8 +226,14 @@ export class FichaArticuloComponent {
       this.formArticulo.get('tiene_lote')!.disable();
     }
 
-    this.formArticulo.get('formato')!.disable();
+    if (this.formArticulo.get('id_familia')!.value === -1) {
+      this.formArticulo.get('id_subfamilia')!.disable();
+    }
 
+    this.formArticulo.get('formato')!.disable();
+  }
+
+  tratamientoPreFormulario() {
     return {
       id_articulo: this.articulo.id_articulo,
       fecha_alta: moment(this.articulo.fecha_alta).format('DD/MM/yyyy'),
@@ -241,6 +261,7 @@ export class FichaArticuloComponent {
   }
 
   editarCampo<K extends keyof Articulo>(campo: K) {
+    console.log('entra');
 
     const camposSinDelay = ['activo', 'tiene_lote', 'tipo']
 
@@ -331,14 +352,29 @@ export class FichaArticuloComponent {
             case 'id_familia':
 
               this.listasDesplegables.subfamilia = null;
+              this.articulo.id_subfamilia = -1;
+              this.formArticulo.get('id_subfamilia')!.setValue(-1);
+
+              from(this._supabase.supabase.from('articulos').update({ id_subfamilia: null }).eq('id_articulo', this.formArticulo.get('id_articulo')!.value)).subscribe(({ data, error }) => {
+                if (error) {
+                  this._supabase.anadirLog(`ha tenido un error al modificar el campo "subfamilia" del artículo con código ${this.id}`, error.message);
+                } else {
+                  this._supabase.anadirLog(`ha modificado el campo "subfamilia": ${this.valoresAnteriores['id_subfamilia']} \u2192 "" del artículo con código ${this.id}`);
+                }
+              });
 
               from(this._supabase.supabase.from('subfamilias').select('*').eq('id_familia', this.formArticulo.get('id_familia')!.value)).subscribe(({ data, error }) => {
                 if (!error) {
                   const subfamilias = (data ?? []) as Subfamilia[];
                   this.listasDesplegables = { ...this.listasDesplegables, subfamilia: subfamilias.map(subfamilia => ({ codigo: subfamilia.id_subfamilia, nombre: subfamilia.nombre })) } as unknown as ListaDesplegablesFichaArticulo;
+                  this.filtrarDesplegableSearch('subfamilia');
                 }
               });
 
+              break;
+
+            case 'comision_default':
+              this.comisionDefaultDialog = Number(this.formArticulo.get(campo)!.value);
               break;
           }
         }
@@ -370,5 +406,58 @@ export class FichaArticuloComponent {
         }
       }
     })
+  }
+
+  async abrirDialogEditarComisiones() {
+
+    this.dialogRef = this._dialog.open(this.dialogEditarComision);
+
+    const { data, error } = await this._supabase.supabase.from('vendedores').select('*').order('nombre');
+
+    if (error) {
+      //TODO: enseñar spinner, gestionar error
+    } else {
+      this.listaVendedoresDialog = data?.map(vendedor => { return { codigo: vendedor.id_vendedor, nombre: vendedor.nombre, comision: 0 } })!;
+
+      this.listaVendedores.forEach((vendedor) => {
+        const vend = this.listaVendedoresDialog.find(vendedor2 => vendedor.nombre === vendedor2.nombre);
+        if (vend) vend.comision = vendedor.comision;
+      });
+    }
+  }
+
+  async modificarComision(vendedor: Vendedor) {
+    //TODO: meter logs
+    vendedor.comision = Number(vendedor.comision);
+
+    //TODO: meter error formato en input comision_default en el dialog (no funciona porque ahora es ngmodel)
+
+    const { data, error } = await this._supabase.supabase.from('comisiones_articulos').select('*').eq('id_articulo', this.articulo.id_articulo).eq('id_vendedor', vendedor.codigo);
+
+    if (data!.length === 0) {
+      const { error } = await this._supabase.supabase.from('comisiones_articulos').insert({ id_articulo: this.id, id_vendedor: vendedor.codigo, comision: vendedor.comision });
+      if (error) {
+        //TODO: gestión error
+      }
+    } else if (data!.length === 1) {
+      if (vendedor.comision === 0) {
+        const { error } = await this._supabase.supabase.from('comisiones_articulos').delete().eq('id_articulo', this.id).eq('id_vendedor', vendedor.codigo);
+        if (error) {
+          //TODO: gestión error
+        }
+      } else {
+        const { error } = await this._supabase.supabase.from('comisiones_articulos').update({ comision: vendedor.comision }).eq('id_articulo', this.id).eq('id_vendedor', vendedor.codigo);
+        if (error) {
+          //TODO: gestión error
+        }
+      }
+    }
+    this.getVendedores();
+    this.inputVendedor.setValue('');
+  }
+
+  editarComisionDefaultDialog() {
+    this.formArticulo.get('comision_default')!.setValue(Number(this.comisionDefaultDialog));
+    this.editarCampo('comision_default')
   }
 }
